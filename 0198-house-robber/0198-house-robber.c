bc721260943c9014fc26bc0a21064d921b6cc33c
Vishnu Mashalkar <vishnumashalkar.cs24@bmsce.ac.in>
2026-05-22 21:41:48 +0530

    Time: 0 ms (100%), Space: 8.8 MB (64.65%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.c
@@ -0,0 +1,16 @@
+int rob(int* nums, int numsSize) {
+    if (numsSize == 1)
+        return nums[0];
+
+    int dp[numsSize];
+    dp[0] = nums[0];
+    dp[1] = (nums[0] > nums[1]) ? nums[0] : nums[1];
+
+    for (int i = 2; i < numsSize; i++) {
+        int take = nums[i] + dp[i - 2];
+        int skip = dp[i - 1];
+        dp[i] = (take > skip) ? take : skip;
+    }
+
+    return dp[numsSize - 1];
+}
