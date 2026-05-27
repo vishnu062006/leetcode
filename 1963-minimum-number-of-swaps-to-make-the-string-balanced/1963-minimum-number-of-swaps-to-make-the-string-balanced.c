5a389f5a3eae1dd1cb544753cf18c8c9c4fb0800
Vishnu Mashalkar <vishnumashalkar.cs24@bmsce.ac.in>
2026-05-27 23:27:50 +0530

    Time: 16 ms (8.57%), Space: 16.3 MB (5.71%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.c
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.c
@@ -1,3 +1,15 @@
 int minSwaps(char* s) {
-    
+    int open=0;
+    int close=0;
+    int count=0;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='[')   open++;
+        if(s[i]==']')   close++;
+
+        if(close-open>0){    
+            count++;
+            close-=2;
+        }
+    }
+    return count;
 }
