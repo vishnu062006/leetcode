7470d3638086eede6c708cafb75967ccb5fc48ed
Vishnu Mashalkar <vishnumashalkar.cs24@bmsce.ac.in>
2026-06-11 18:36:27 +0530

    Time: 108 ms (52.38%), Space: 281.6 MB (54.76%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
@@ -0,0 +1,60 @@
+class Solution {
+    static final long MOD = 1_000_000_007L;
+
+    public int assignEdgeWeights(int[][] edges) {
+        int n = edges.length + 1;
+
+        List<Integer>[] adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n + 1];
+
+        q.offer(1);
+        vis[1] = true;
+
+        int depth = -1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            depth++;
+
+            for (int i = 0; i < size; i++) {
+                int node = q.poll();
+
+                for (int nei : adj[node]) {
+                    if (!vis[nei]) {
+                        vis[nei] = true;
+                        q.offer(nei);
+                    }
+                }
+            }
+        }
+
+        return (int) modPow(2, depth - 1);
+    }
+
+    private long modPow(long base, long exp) {
+        long ans = 1;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                ans = (ans * base) % MOD;
+            }
+
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+
+        return ans;
+    }
+}
