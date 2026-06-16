f0913a13fb5fb546bd9f64436c5f87b63115b082
Vishnu Mashalkar <vishnumashalkar.cs24@bmsce.ac.in>
2026-06-16 20:22:55 +0530

    Time: 4 ms (76.47%), Space: 55.4 MB (48.53%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                result.append(ch);
+            } else if (ch == '*') {
+                if (result.length() > 0) {
+                    result.deleteCharAt(result.length() - 1);
+                }
+            } else if (ch == '#') {
+                result.append(result.toString());
+            } else if (ch == '%') {
+                result.reverse();
+            }
+        }
+
+        return result.toString();
+    }
+}
