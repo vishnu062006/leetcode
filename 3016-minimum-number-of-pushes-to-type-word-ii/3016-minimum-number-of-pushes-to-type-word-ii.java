7f3446495821730777478810bba97c70319c61c3
Vishnu Mashalkar <vishnumashalkar.cs24@bmsce.ac.in>
2026-07-31 12:08:47 +0530

    Time: 10 ms (66.76%), Space: 47.9 MB (87.86%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int ans = 0;
+        int push = 1;
+        int cnt = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) 
+                break;
+
+            ans += freq[i] * push;
+            cnt++;
+
+            if (cnt == 8) {
+                cnt = 0;
+                push++;
+            }
+        }
+
+        return ans;
+    }
+}
